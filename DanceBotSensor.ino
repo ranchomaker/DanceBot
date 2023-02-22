6dc548123d25063227e717046c41a722a8cc70e2
adampely <adampely@users.noreply.github.com>
2023-02-22 08:39:27 -0800

    Changed to analog pins and this code should actually run

--- DanceBotSensor.ino
+++ DanceBotSensor.ino
@@ -1,18 +1,24 @@
 
 // Define the pin modes
-const int trigPin = 12;
-const int echoPin = 11;
+const int trigPin = 2;
+const int echoPin = 3;
+const int l1 = 5;
+const int l2 = 6;
+const int r1 = 9;
+const int r2 = 10;
 const int maxDistance = 50; // Maximum distance (in cm) that the sensor can detect
-const int stopDistance = 5; // Distance in cm at which the vehicle should stop
+const long stopDistance = 20.0; // Distance in cm at which the vehicle should stop
 
 // Set the mode of the pin (output or input)
 void setup() {
+  Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(4, OUTPUT); // Motor 1 forward
-  pinMode(5, OUTPUT); // Motor 1 backward
-  pinMode(6, OUTPUT); // Motor 2 forward
-  pinMode(7, OUTPUT); // Motor 2 backward
+  pinMode(l1, OUTPUT); // Motor 1 forward
+  pinMode(l2, OUTPUT); // Motor 1 backward
+  pinMode(r1, OUTPUT); // Motor 2 forward
+  pinMode(r2, OUTPUT); // Motor 2 backward
+  delay(1000);
 }
 
 void loop() {
@@ -23,43 +29,65 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
+  distance = (duration/2.0) / 29.1;
+  Serial.print(distance);
+  Serial.println();
+  delay(15);
   if (distance <= stopDistance) {
     stopVehicle();
     delay(1000);
-    turnRight();
-    delay(1000);
-    moveForward();
+    if (random(0,2) == 1){  // Turn left or right randomely
+      turnRight();
+    }
+    else{
+      turnLeft();
+    }
+    
+    delay(random(100, 250));
+    stopVehicle();
+    delay(500);
+    // moveForward();
+    // delay(1000);
   }
   else {
+    Serial.print("moving forward");
+    Serial.println();
     moveForward();
   }
 }
 
 void moveForward() {
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
+  // digitalWrite(l1, HIGH);
+  //digitalWrite(l2, LOW);
+  //digitalWrite(r1, HIGH);
+  //digitalWrite(r2, LOW);
+  // the left wheel and right wheel are set differently because the motor seem to output different amounts of power.
+  // These ratios might need to be tuned on a per car basis? 
+  // This is currently set to go at a pretty slow speed. Set it too fast and the car will crash into a wall before it can stop
+  // from pure momentum. 
+  analogWrite(l1, 105);  
+  analogWrite(l2, 0);
+  analogWrite(r1, 90);
+  analogWrite(r2, 0);
 }
 
 void stopVehicle() {
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
+  digitalWrite(l1, LOW);
+  digitalWrite(l2, LOW);
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
 }
 
 void turnRight() {
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
+  digitalWrite(l1, HIGH);
+  digitalWrite(l2, LOW);
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
 }
 
 void turnLeft() {
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
+  digitalWrite(l1, LOW);
+  digitalWrite(l2, LOW);
+  digitalWrite(r1, HIGH);
+  digitalWrite(r2, LOW);
 }
